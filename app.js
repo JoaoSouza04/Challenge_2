07ebf1c405c826fb09edb8410aec7ae168ce5032
João Pedro <joaosvieira04@gmail.com>
2023-02-02 11:02:30 -0300

    Criando meu arquivo principal e inserindo as configurações básicas

---
+++ app.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+app.use(express.json())
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE.PASSWORD
+)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Database successfully connected!')
+  })
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log('Listening on port 3000')
+})
