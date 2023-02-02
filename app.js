a3e32058d02cadb00521b57e6b8383ad5d0f3eb3
João Pedro <joaosvieira04@gmail.com>
2023-02-02 12:37:38 -0300

    Subindo meus arquivos de configuração da sintaxe

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
-app.use(express.json())
+const mongoose = require('mongoose');
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+app.use(express.json());
 
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-)
+);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
   .then(() => {
-    console.log('Database successfully connected!')
-  })
+    console.log('Database successfully connected!');
+  });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port 3000')
-})
+  console.log('Listening on port 3000');
+});
