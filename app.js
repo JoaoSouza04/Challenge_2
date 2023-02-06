6b8b3e2864bca34d7094381306a99c8f9d6542f2
João Pedro <joaosvieira04@gmail.com>
2023-02-06 08:44:45 -0300

    utilizando e testando os controllers

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const eventController = require('./controllers/event.Controller.js');
+const { Router } = require('express');
 app.use(express.json());
 
 dotenv.config({ path: './config.env' });
@@ -21,7 +23,11 @@ mongoose
     console.log('Database successfully connected!');
   });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.post('/api/v1', eventController.createEvent);
+app.get('/api/v1', eventController.getEvents);
+app.get('/api/v1/events/:id', eventController.getEventById);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('Listening on port 3000');
 });
