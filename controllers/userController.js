44a7d4737d7e16112ba0f8a1f4993f06aae52d5e
João Pedro <joaosvieira04@gmail.com>
2023-02-07 22:33:13 -0300

    Arrumando alguns erros

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,18 @@ const User = require('../models/userModel');
 
 exports.signUp = async (req, res) => {
   try {
-    if (!req.body) return res.send('Please type the fields!');
+    if (!req.body) return res.status(400).send('Please type the fields!');
 
     let date = new Date(req.body.birthDate);
-    date = date.getFullYear();
-    if (date <= 1910) return res.send('Invalid Date!');
+    if (date.getMonth() === undefined || date.getDate() === undefined)
+      return res.status(400).send('Please provide the month and the day!');
 
+    if (date.getFullYear() <= 1910)
+      return res.status(400).send('Invalid Date!');
+
+    let checkPassword = req.body.password;
+    if (checkPassword.length < 8)
+      return res.status(400).send('The password is too short!');
     const user = await User.create(req.body);
 
     res.status(201).json({
@@ -26,12 +32,15 @@ exports.signUp = async (req, res) => {
 exports.signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) return res.send('Please provide the fields!');
+    if (!email || !password)
+      return res.status(400).send('Please provide the fields!');
 
     const user = await User.findOne({ email }).select('+password');
     const userPassword = user.password;
 
-    if (userPassword !== password) return res.send('The password is wrong!');
+    if (userPassword !== password)
+      return res.status(400).send('The password is wrong!');
+
     res.status(200).json({
       message: 'that is your user!',
       data: {
@@ -42,5 +51,41 @@ exports.signIn = async (req, res) => {
     res.status(404).json({
       message: 'User not found!',
     });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    if (!req.params.id || !req.body)
+      return res
+        .status(400)
+        .send('Please type the id or the update content for an User!');
+
+    if (req.body.firstName === '' || req.body.lastName === '')
+      return res.status(400).send('Please provide your name!');
+
+    let checkPassword = req.body.password;
+    if (checkPassword.length < 8)
+      return res.status(400).send('The password is too short!');
+
+    if (req.body.password != req.body.passwordConfirm)
+      return res.status(400).send('Passwords are not equal!');
+
+    const userFound = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: false,
+    });
+
+    res.status(200).json({
+      message: 'Event updated!',
+      data: {
+        User: userFound,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: `User wasn't updated`,
+    });
+    console.log(err);
   }
 };
